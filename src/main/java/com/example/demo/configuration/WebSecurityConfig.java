6b951c9ea97a920837120290a753d69bdec78d88
blinky-z <dm.blink19@gmail.com>
2019-03-19 22:11:59 +0300

    Update front

--- src/main/java/com/example/demo/configuration/WebSecurityConfig.java
+++ src/main/java/com/example/demo/configuration/WebSecurityConfig.java
@@ -33,7 +33,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "/css/**", "/js/**", "/images/**").permitAll()
+                .antMatchers("/css/**", "/js/**", "/images/**").permitAll()
                 .anyRequest().authenticated()
 
                 .and()
