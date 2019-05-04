faa8414b47ee5def2fcfba26312b5f699db07871
blinky-z <dm.blink19@gmail.com>
2019-05-04 17:22:45 +0300

    Change tests configuration: now Local File System backup folder is located in OS's temp folder

--- src/test/java/com/blog/TestConfiguration.java
+++ src/test/java/com/blog/TestConfiguration.java
@@ -6,8 +6,8 @@ import com.blog.entities.database.PostgresSettings;
 import com.blog.entities.storage.DropboxSettings;
 import com.blog.entities.storage.LocalFileSystemSettings;
 import com.blog.entities.storage.StorageSettings;
-import com.blog.repositories.DatabaseSettingsRepository;
-import com.blog.repositories.StorageSettingsRepository;
+import com.blog.manager.DatabaseSettingsManager;
+import com.blog.manager.StorageSettingsManager;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,8 +18,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
+import java.io.IOException;
 import java.net.URI;
-import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.DatabaseMetaData;
 import java.sql.SQLException;
 import java.util.Objects;
@@ -28,12 +30,11 @@ import java.util.Objects;
 public class TestConfiguration {
     private static final Logger logger = LoggerFactory.getLogger(TestConfiguration.class);
     private static final String dropboxAccessToken = "tzFnUqsYFXAAAAAAAAAAG-irDd6KaODXHm7TlYvPwBytOxGRTJz-F0u4grmndSg3";
-    private static final String localFileSystemBackupPath =
-            FileSystems.getDefault().getPath("src/test").toAbsolutePath().toString();
+
     @Autowired
-    private StorageSettingsRepository storageSettingsRepository;
+    private StorageSettingsManager storageSettingsManager;
     @Autowired
-    private DatabaseSettingsRepository databaseSettingsRepository;
+    private DatabaseSettingsManager databaseSettingsManager;
     @Autowired
     @Qualifier("masterPostgresDataSource")
     private DataSource masterPostgresDataSource;
@@ -54,7 +55,7 @@ public class TestConfiguration {
                 PostgresSettings postgresSettings = new PostgresSettings();
                 URI parsedConnUrl = URI.create(connUrl);
 
-                DatabaseSettings postgresDatabaseSettings = DatabaseSettings.postgresSettings(postgresSettings)
+                createdDatabaseSettings = DatabaseSettings.postgresSettings(postgresSettings)
                         .withHost(parsedConnUrl.getHost())
                         .withPort(parsedConnUrl.getPort())
                         .withDatabaseName(parsedConnUrl.getPath().substring(parsedConnUrl.getPath().lastIndexOf("/") + 1))
@@ -62,8 +63,6 @@ public class TestConfiguration {
                         .withPassword("postgres")
                         .withSettingsName(settingsName)
                         .build();
-
-                createdDatabaseSettings = databaseSettingsRepository.save(postgresDatabaseSettings);
                 break;
             }
             default: {
@@ -74,7 +73,7 @@ public class TestConfiguration {
         logger.info("Initializing database settings completed. Database type: {}. " +
                 "Created Database settings: {}", DatabaseType.POSTGRES, createdDatabaseSettings);
 
-        return Objects.requireNonNull(createdDatabaseSettings);
+        return createdDatabaseSettings;
     }
 
     @Bean
@@ -89,31 +88,39 @@ public class TestConfiguration {
 
     @Bean
     public DatabaseSettings masterPostgresDatabaseSettings() throws SQLException {
-        return buildDatabaseSettings(DatabaseType.POSTGRES, "masterPostgresTestDatabaseSettings",
-                masterPostgresDataSource);
+        return Objects.requireNonNull(databaseSettingsManager.save(
+                buildDatabaseSettings(DatabaseType.POSTGRES, "masterPostgresTestDatabaseSettings",
+                        masterPostgresDataSource)));
     }
 
     @Bean
     public DatabaseSettings copyPostgresDatabaseSettings() throws SQLException {
-        return buildDatabaseSettings(DatabaseType.POSTGRES, "copyPostgresTestDatabaseSettings",
-                copyPostgresDataSource);
+        return Objects.requireNonNull(databaseSettingsManager.save(
+                buildDatabaseSettings(DatabaseType.POSTGRES, "copyPostgresTestDatabaseSettings",
+                        copyPostgresDataSource)));
     }
 
     @Bean
     public StorageSettings dropboxStorageSettings() {
         DropboxSettings dropboxSettings = new DropboxSettings();
         dropboxSettings.setAccessToken(dropboxAccessToken);
-        return Objects.requireNonNull(storageSettingsRepository.save(StorageSettings.dropboxSettings(dropboxSettings)
-                .withSettingsName("testDropboxStorageSettings")
-                .build()));
+
+        return Objects.requireNonNull(storageSettingsManager.save(
+                StorageSettings.dropboxSettings(dropboxSettings)
+                        .withSettingsName("testDropboxStorageSettings")
+                        .build()));
     }
 
     @Bean
-    public StorageSettings localFileSystemStorageSettings() {
+    public StorageSettings localFileSystemStorageSettings() throws IOException {
         LocalFileSystemSettings localFileSystemSettings = new LocalFileSystemSettings();
-        localFileSystemSettings.setBackupPath(localFileSystemBackupPath);
-        return Objects.requireNonNull(
-                storageSettingsRepository.save(StorageSettings.localFileSystemSettings(localFileSystemSettings)
+        Path tempDir = Files.createTempDirectory("dbBackupTests");
+        String tempDirPath = tempDir.toAbsolutePath().toString();
+        logger.info("Created temporary folder for File System Storage tests. Path: {}", tempDirPath);
+        localFileSystemSettings.setBackupPath(tempDirPath);
+
+        return Objects.requireNonNull(storageSettingsManager.save(
+                StorageSettings.localFileSystemSettings(localFileSystemSettings)
                         .withSettingsName("testLocalFileSystemStorageSettings")
                         .build()));
     }
