4b23d3e6f6ec1d912d9fbdcd6261aa186941dcd0
blinky-z <dm.blink19@gmail.com>
2019-05-09 01:18:36 +0300

    Add BackupTaskRepository

---
+++ src/main/java/com/blog/repositories/BackupTaskRepository.java
@@ -0,0 +1,8 @@
+package com.blog.repositories;
+
+import com.blog.entities.backup.BackupTask;
+import org.springframework.data.repository.CrudRepository;
+
+public interface BackupTaskRepository extends CrudRepository<BackupTask, Integer> {
+    Iterable<BackupTask> findAllByOrderByDateDesc();
+}
