5be0674b02375b76efaf5030deb66fd60e598e8a
blinky-z <dm.blink19@gmail.com>
2019-05-23 15:47:36 +0300

    Update WebApiDatabaseControllerTests
    1. Add matching of equal content of DTO and Entity in adding storage tests
    2. Add new test covering deletion of DatabaseSettings

--- src/test/java/com/blog/WebApiTests/ControllerTests/WebApiDatabaseControllerTests.java
+++ src/test/java/com/blog/WebApiTests/ControllerTests/WebApiDatabaseControllerTests.java
@@ -1,9 +1,13 @@
 package com.blog.WebApiTests.ControllerTests;
 
 import com.blog.ApplicationTests;
+import com.blog.entities.database.DatabaseSettings;
 import com.blog.entities.database.DatabaseType;
 import com.blog.repositories.DatabaseSettingsRepository;
 import com.blog.webUI.formTransfer.WebAddDatabaseRequest;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
@@ -15,6 +19,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
@@ -36,6 +43,10 @@ class WebApiDatabaseControllerTests extends ApplicationTests {
         controllersHttpClient.login();
     }
 
+    static Matcher<DatabaseSettings> isEqualToDto(WebAddDatabaseRequest dto) {
+        return new equalsToDto(dto);
+    }
+
     @Test
     void createDatabase_ShouldSavePostgresSettingsIntoDatabase_WhenGotRequestToSaveProperPostgresSettings() {
         String settingsName =
@@ -46,18 +57,32 @@ class WebApiDatabaseControllerTests extends ApplicationTests {
 
         controllersHttpClient.addDatabase(request);
 
-        assertTrue(databaseSettingsRepository.existsById(settingsName));
+        Optional<DatabaseSettings> optionalDatabaseSettings = databaseSettingsRepository.findById(settingsName);
+        assertTrue(optionalDatabaseSettings.isPresent());
+        assertThat(optionalDatabaseSettings.get(), isEqualToDto(request));
     }
 
     @Test
-    void deleteDatabase_ShouldRespondWith400Error_WhenDatabaseSettingsNameNotProvided() {
-        ResponseEntity<String> responseEntity = controllersHttpClient.deleteDatabase(null);
-        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    void deleteDatabase_ShouldDeleteSettings_WhenSendProperRequest() {
+        String settingsName =
+                "createDatabase_ShouldSavePostgresSettingsIntoDatabase_WhenGotRequestToSaveProperPostgresSettings";
+
+        // create settings to subsequent delete
+        {
+            WebAddDatabaseRequest request = controllersHttpClient.buildDefaultAddDatabaseRequest(
+                    DatabaseType.POSTGRES, settingsName);
+
+            controllersHttpClient.addDatabase(request);
+        }
+
+        controllersHttpClient.deleteStorage(settingsName);
+
+        assertFalse(databaseSettingsRepository.existsById(settingsName));
     }
 
     @Test
-    void deleteDatabase_ShouldRespondWithFound_WhenDatabaseSettingsNameProvidedButNoSuchSettingsToDelete() {
-        String settingsName = "deleteDatabase_ShouldRespondWithFound_WhenDatabaseSettingsNameProvidedButNoSuchSettingsToDelete";
+    void deleteDatabase_ShouldNotRespondWithError_WhenDatabaseSettingsNameProvidedButNoSuchSettingsToDelete() {
+        String settingsName = "deleteDatabase_ShouldNotRespondWithError_WhenDatabaseSettingsNameProvidedButNoSuchSettingsToDelete";
 
         // create settings
         {
@@ -73,10 +98,40 @@ class WebApiDatabaseControllerTests extends ApplicationTests {
             assertFalse(databaseSettingsRepository.existsById(settingsName));
         }
 
-        // try to delete settings again
+        // delete settings again
         {
             ResponseEntity<String> responseEntity = controllersHttpClient.deleteDatabase(settingsName);
             assertEquals(HttpStatus.FOUND, responseEntity.getStatusCode());
+        }
+    }
+
+    @Test
+    void deleteDatabase_ShouldRespondWith400Error_WhenDatabaseSettingsNameNotProvided() {
+        ResponseEntity<String> responseEntity = controllersHttpClient.deleteDatabase(null);
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+    }
+
+    private static final class equalsToDto extends TypeSafeMatcher<DatabaseSettings> {
+        private WebAddDatabaseRequest dto;
+
+        equalsToDto(WebAddDatabaseRequest dto) {
+            this.dto = dto;
+        }
+
+        @Override
+        protected boolean matchesSafely(DatabaseSettings entity) {
+            return entity.getSettingsName().equals(dto.getSettingsName()) &&
+                    entity.getType().equals(DatabaseType.of(dto.getDatabaseType()).get()) &&
+                    entity.getName().equals(dto.getDatabaseName())
+                    && entity.getHost().equals(dto.getHost())
+                    && entity.getPort() == Integer.valueOf(dto.getPort()) &&
+                    entity.getLogin().equals(dto.getLogin())
+                    && entity.getPassword().equals(dto.getPassword());
+        }
+
+        @Override
+        public void describeTo(Description description) {
+            description.appendText("Entity should be equal to DTO: " + dto.toString());
         }
     }
 }
